785adb945f3d63d0d064cf99cacf53a7798f5eb8
Mahamud Hasan <hasanmukit009@gmail.com>
2021-03-14 23:06:57 +0930

    Error test

--- CDU_Document_Automation/DMS.Web/Startup.cs
+++ CDU_Document_Automation/DMS.Web/Startup.cs
@@ -49,6 +49,7 @@ namespace DMS.Web
             }
             else
             {
+                app.UseDeveloperExceptionPage();
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
